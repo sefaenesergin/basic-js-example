4f64ef3bc2f1b935e655823d119a2e194e4f4e0a
sefaenesergin <mnstrlap@gmail.com>
2020-10-01 11:57:26 +0300

    initial

---
+++ script.js
@@ -0,0 +1,16 @@
+
+      // JavaScript goes here
+        document.addEventListener("DOMContentLoaded", function() {
+        function createParagraph() {
+            let para = document.createElement('p');
+            para.textContent = 'TIKLADIM!';
+            document.body.appendChild(para);
+        }
+
+        const buttons = document.querySelectorAll('button');
+
+        for(let i = 0; i < buttons.length ; i++) {
+            buttons[i].addEventListener('click', createParagraph);
+        }
+        });
+        alert("Hello World!")
