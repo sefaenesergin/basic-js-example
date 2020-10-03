7ce24bbcdc1150de1454a4a7ff5220c06d523252
sefaenesergin <mnstrlap@gmail.com>
2020-10-03 16:25:41 +0300

    add something

--- script.js
+++ script.js
@@ -1,8 +1,60 @@
-var Number1,Number2,Operator,result,answer,true=0,false=0;
- Number1=document.getElementById('Number1');
- Number2=document.getElementById('Number2');
- Operator=document.getElementById('Operator');
- result=document.getElementById('result');
- answer=document.getElementById('answer'); 
- true=document.getElementById('true'); 
- false=document.getElementById('false'); 
+var d = new Date();
+document.getElementById("Time").innerHTML=d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+
+var Number1,Number2,Operator,result,answer,True=0,False=0;
+
+//Accessing HTML objects...
+Number1=document.getElementById('Number1');
+Number2=document.getElementById('Number2');
+Operator=document.getElementById('Operator');
+result=document.getElementById('result');
+answer=document.getElementById('answer'); 
+True=document.getElementById('True'); 
+False=document.getElementById('False'); 
+
+
+//Generate random numbers function
+function RandomNumber(min,max){
+    var number=Math.floor(Math.random()*(max-min))+min;
+    return number;
+}
+
+function newQuestion(){
+    var operation=["+","-","*","/"];
+    Operator.textContent=operation[RandomNumber(0,4)];//Choose operator
+    Number1.textContent=RandomNumber(0,50);
+    Number2.textContent=RandomNumber(0,50);
+    if(Operator.textContent=="/"){
+        while(true){
+                Number2.textContent=RandomNumber(0,50);
+                if(Number1.textContent%Number2.textContent==0){
+                    break;
+                }
+        }
+    }
+}
+
+window.onload=function(){
+    newQuestion();
+}
+
+answer.onclick=function(){
+    var ans,n1,n2;
+    n1=Number(Number1.textContent);
+    n2=Number(Number2.textContent);
+    switch(Operator.textContent){
+            case '+': ans= n1+n2 ;break;
+            case '-': ans= n1-n2 ; break;
+            case '*': ans= n1*n2 ; break;
+            case '/': ans= n1/n2 ;break;
+            default: break;
+    }
+    
+    if(result.value==ans){
+        True.textContent=Number(True.textContent)+1;
+    }else{
+        False.textContent=Number(False.textContent)+1;
+    }
+
+    newQuestion();
+}
